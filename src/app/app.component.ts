a6f093c445158c4297c4df809f2f6e17f8beaeb8
unknown <th.barry@yahoo.com>
2020-10-20 22:56:32 +0200

    initialize projet

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { OnInit } from "@angular/core";
+
+import { Pokemon } from "./pokemon";
+import { POKEMONS } from "./mock-pokemons";
+
+@Component({
+    selector: 'pokemon-app',
+    templateUrl: './app/app-component.html'
+})
+
+export class AppComponent  implements OnInit{ 
+
+    private pokemons: Pokemon[];
+    private titre: string = "Liste des pokémons";
+    private value: string = "";
+
+    /** plutôt conseiller d'initialiser les données 
+     venant de l'exterieurs(server, base de données)
+     que dans le constructeur.
+    */
+    ngOnInit(){
+        this.pokemons = POKEMONS;
+    }
+    onClick(){
+        console.log('cliquer');
+    }
+    onKey(event:any){
+         this.value = "Bonjour "+ event.target.value;
+    }
+    selectPokemon(pokemon: Pokemon){
+       alert("vous avez cliquer sur "+pokemon.name);
+    }
+ }
